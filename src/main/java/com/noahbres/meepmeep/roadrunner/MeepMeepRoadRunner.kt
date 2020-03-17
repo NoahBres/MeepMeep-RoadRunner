a2ffae6a53c6bb18c1db9246425f083e886aa464
Noah <noahbresler2002@gmail.com>
2020-03-17 06:26:16 -0500

    feat(MeepMeepRoadRunner.kt): remove entity on start

--- src/main/java/com/noahbres/meepmeep/roadrunner/MeepMeepRoadRunner.kt
+++ src/main/java/com/noahbres/meepmeep/roadrunner/MeepMeepRoadRunner.kt
@@ -2,6 +2,16 @@ package com.noahbres.meepmeep.roadrunner
 
 import com.noahbres.meepmeep.core.MeepMeep
 
-class MeepMeepRoadRunner(windowSize: Int): MeepMeep(windowSize) {
+class MeepMeepRoadRunner(windowSize: Int): MeepMeep<MeepMeepRoadRunner>(windowSize) {
+    override fun start(): MeepMeepRoadRunner {
+       super.start()
 
+        removeEntity(DEFAULT_BOT_ENTITY)
+
+        return this
+    }
+
+    fun addTrajectory(): MeepMeepRoadRunner {
+        return this
+    }
 }
