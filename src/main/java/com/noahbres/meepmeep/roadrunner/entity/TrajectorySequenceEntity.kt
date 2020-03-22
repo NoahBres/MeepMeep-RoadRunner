66aa1be76842ddddcd6b04993ce6c97f074b2980
Noah <noahbresler2002@gmail.com>
2020-03-22 05:24:38 -0500

    feat(TurnIndicatorEntity.kt): move turn indicator drawing to it's own entity

--- src/main/java/com/noahbres/meepmeep/roadrunner/entity/TrajectorySequenceEntity.kt
+++ src/main/java/com/noahbres/meepmeep/roadrunner/entity/TrajectorySequenceEntity.kt
@@ -1,11 +1,10 @@
 package com.noahbres.meepmeep.roadrunner.entity
 
 import com.acmerobotics.roadrunner.geometry.Vector2d
-import com.noahbres.meepmeep.core.*
+import com.noahbres.meepmeep.core.MeepMeep
 import com.noahbres.meepmeep.core.colorscheme.ColorScheme
 import com.noahbres.meepmeep.core.entity.ThemedEntity
 import com.noahbres.meepmeep.core.util.FieldUtil
-import com.noahbres.meepmeep.roadrunner.*
 import com.noahbres.meepmeep.roadrunner.toScreenCoord
 import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence
 import com.noahbres.meepmeep.roadrunner.trajectorysequence.sequencestep.TrajectoryStep
@@ -13,8 +12,6 @@ import com.noahbres.meepmeep.roadrunner.trajectorysequence.sequencestep.TurnStep
 import java.awt.*
 import java.awt.geom.Path2D
 import java.awt.image.BufferedImage
-import kotlin.math.abs
-import kotlin.math.min
 import kotlin.math.roundToInt
 
 class TrajectorySequenceEntity(
@@ -31,13 +28,6 @@ class TrajectorySequenceEntity(
     private val PATH_OUTER_STROKE_WIDTH = 2.0
 
     private val PATH_OUTER_OPACITY = 0.4
-
-    private val TURN_CIRCLE_WIDTH = 1.0
-    private val TURN_ARC_WIDTH = 7.0
-    private val TURN_STROKE_WIDTH = 0.5
-    private val TURN_ARROW_LENGTH = 1.5
-    private val TURN_ARROW_ANGLE = 30.0.toRadians()
-    private val TURN_ARROW_ANGLE_ADJUSTMENT = -12.5.toRadians()
 
     private val SAMPLE_RESOLUTION = 1.2
 
@@ -57,8 +47,6 @@ class TrajectorySequenceEntity(
 
         gfx.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
         gfx.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
-
-        val turnIndicatorList = mutableListOf<TurnIndicator>()
 
         val trajectoryDrawnPath = Path2D.Double()
 
@@ -89,7 +77,8 @@ class TrajectorySequenceEntity(
 
                 currentEndPose = step.trajectory.end()
             } else if (step is TurnStep) {
-                turnIndicatorList.add(TurnIndicator(currentEndPose.vec(), currentEndPose.heading, currentEndPose.heading + step.angle))
+                val turnEntity = TurnIndicatorEntity(meepMeep, colorScheme, currentEndPose.vec(), currentEndPose.heading, currentEndPose.heading + step.angle)
+                meepMeep.addEntity(turnEntity)
             }
         }
 
@@ -100,44 +89,6 @@ class TrajectorySequenceEntity(
         gfx.stroke = innerStroke
         gfx.color = colorScheme.TRAJCETORY_PATH_COLOR
         gfx.draw(trajectoryDrawnPath)
-
-        gfx.color = colorScheme.TRAJECTORY_TURN_COLOR
-        gfx.stroke = BasicStroke(TURN_STROKE_WIDTH.scaleInToPixel().toFloat(), BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)
-
-        turnIndicatorList.forEach {
-            gfx.fillOval(
-                    (it.pos.toScreenCoord().x - TURN_CIRCLE_WIDTH.scaleInToPixel() / 2).toInt(),
-                    (it.pos.toScreenCoord().y - TURN_CIRCLE_WIDTH.scaleInToPixel() / 2).toInt(),
-                    TURN_CIRCLE_WIDTH.scaleInToPixel().toInt(), TURN_CIRCLE_WIDTH.scaleInToPixel().toInt()
-            )
-
-            gfx.drawArc(
-                    (it.pos.toScreenCoord().x - TURN_ARC_WIDTH.scaleInToPixel() / 2).toInt(),
-                    (it.pos.toScreenCoord().y - TURN_ARC_WIDTH.scaleInToPixel() / 2).toInt(),
-                    TURN_ARC_WIDTH.scaleInToPixel().toInt(), TURN_ARC_WIDTH.scaleInToPixel().toInt(),
-                    min(it.startAngle.toDegrees().toInt(), it.endAngle.toDegrees().toInt()),
-                    abs(it.startAngle.toDegrees().toInt() - it.endAngle.toDegrees().toInt())
-            )
-
-            val arrowPointVec = Vector2d(TURN_ARC_WIDTH / 2, 0.0).rotated(it.endAngle)
-            val translatedPoint = (it.pos + arrowPointVec).toScreenCoord()
-
-            var arrow1Rotated = it.endAngle - 90.0.toRadians() + TURN_ARROW_ANGLE + TURN_ARROW_ANGLE_ADJUSTMENT
-            if (it.endAngle < it.startAngle) arrow1Rotated = 360.0.toRadians() - arrow1Rotated
-
-            var arrow2Rotated = it.endAngle - 90.0.toRadians() - TURN_ARROW_ANGLE + TURN_ARROW_ANGLE_ADJUSTMENT
-            if (it.endAngle < it.startAngle) arrow2Rotated = 360.0.toRadians() - arrow2Rotated
-
-            val arrowEndVec1 = (it.pos + arrowPointVec) + Vector2d(TURN_ARROW_LENGTH, 0.0)
-                    .rotated(arrow1Rotated)
-            val translatedArrowEndVec1 = arrowEndVec1.toScreenCoord()
-
-            val arrowEndVec2 = (it.pos + arrowPointVec) + Vector2d(TURN_ARROW_LENGTH, 0.0).rotated(arrow2Rotated)
-            val translatedArrowEndVec2 = arrowEndVec2.toScreenCoord()
-
-            gfx.drawLine(translatedPoint.x.toInt(), translatedPoint.y.toInt(), translatedArrowEndVec1.x.toInt(), translatedArrowEndVec1.y.toInt())
-            gfx.drawLine(translatedPoint.x.toInt(), translatedPoint.y.toInt(), translatedArrowEndVec2.x.toInt(), translatedArrowEndVec2.y.toInt())
-        }
     }
 
     override fun update(deltaTime: Long) {
