cfa14245d554a3108dc2788db480f1e1f838d2b3
Noah <noahbresler2002@gmail.com>
2020-03-22 03:23:26 -0500

    feat(TrajectorySequence.kt): have TrajectorySequence extends a list

--- src/main/java/com/noahbres/meepmeep/roadrunner/trajectorysequence/TrajectorySequence.kt
+++ src/main/java/com/noahbres/meepmeep/roadrunner/trajectorysequence/TrajectorySequence.kt
@@ -1,18 +1,46 @@
 package com.noahbres.meepmeep.roadrunner.trajectorysequence
 
+import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.noahbres.meepmeep.roadrunner.trajectorysequence.sequencestep.SequenceStep
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.sequencestep.TrajectoryStep
 
-class TrajectorySequence {
+class TrajectorySequence : List<SequenceStep> {
     private val sequenceList = mutableListOf<SequenceStep>()
 
-    val size: Int
-        get() = sequenceList.size
+    fun firstPose(): Pose2d {
+        this.forEach {
+            if(it is TrajectoryStep) return it.trajectory.start()
+        }
+
+        return Pose2d()
+    }
 
     fun add(step: SequenceStep) {
         sequenceList.add(step)
     }
 
-    fun get(i: Int): SequenceStep {
-        return sequenceList[i]
+    override fun contains(element: SequenceStep) = sequenceList.contains(element)
+
+    override fun containsAll(elements: Collection<SequenceStep>) = sequenceList.containsAll(elements)
+
+    override fun indexOf(element: SequenceStep) = sequenceList.indexOf(element)
+
+    override fun isEmpty() = sequenceList.isEmpty()
+
+    override fun iterator() = sequenceList.iterator()
+
+    override fun lastIndexOf(element: SequenceStep) = sequenceList.lastIndexOf(element)
+
+    override fun listIterator() = sequenceList.listIterator()
+
+    override fun listIterator(index: Int) = sequenceList.listIterator(index)
+
+    override fun subList(fromIndex: Int, toIndex: Int) = sequenceList.subList(fromIndex, toIndex)
+
+    override val size: Int
+        get() = sequenceList.size
+
+    override fun get(index: Int): SequenceStep {
+        return sequenceList[index]
     }
 }
