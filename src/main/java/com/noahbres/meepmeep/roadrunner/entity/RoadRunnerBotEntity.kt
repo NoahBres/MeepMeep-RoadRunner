cb9cee1e93ae71e137b062b69afd259201efcf2b
Noah <noahbresler2002@gmail.com>
2020-03-23 19:29:01 -0500

    feat(RoadRunnerBotEntity.kt): finish the trajectorysequence follower

--- src/main/java/com/noahbres/meepmeep/roadrunner/entity/RoadRunnerBotEntity.kt
+++ src/main/java/com/noahbres/meepmeep/roadrunner/entity/RoadRunnerBotEntity.kt
@@ -11,6 +11,7 @@ import com.noahbres.meepmeep.roadrunner.DriveTrainType
 import com.noahbres.meepmeep.roadrunner.toMeepMeepPose
 import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence
 import com.noahbres.meepmeep.roadrunner.trajectorysequence.sequencestep.TrajectoryStep
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.sequencestep.TurnStep
 
 class RoadRunnerBotEntity(
         meepMeep: MeepMeep<*>,
@@ -35,16 +36,45 @@ class RoadRunnerBotEntity(
 
     private var trajectorySequenceEntity: TrajectorySequenceEntity? = null
 
+    private var looping = true
+    private var running = false
+
+    private var trajectorySequenceElapsedTime = 0.0
+
     override fun update(deltaTime: Long) {
+        if (!running) return
+
         if (followMode == FollowMode.TRAJECTORY_LIST) {
 
         } else if (followMode == FollowMode.TRAJECTORY_SEQUENCE && currentTrajectorySequence != null) {
             val currentStep = currentTrajectorySequence?.get(currentTrajectorySequenceIndex)
 
-            if (currentStep is TrajectoryStep) {
-//                println(currentStep.trajectory)
+            trajectorySequenceElapsedTime += deltaTime / 1000.0
+
+            if(trajectorySequenceElapsedTime <= currentTrajectorySequence!!.duration) {
+//                pose = currentTrajectorySequence!!.getPoseAtSeconds(trajectorySequenceElapsedTime).toMeepMeepPose()
+
+                val (currentStateStep, currentStateOffset) = currentTrajectorySequence!!.getCurrentState(trajectorySequenceElapsedTime)
+                when(currentStateStep) {
+                    is TrajectoryStep -> {
+                        pose = currentStateStep.trajectory[currentStateOffset].toMeepMeepPose()
+                    }
+                    is TurnStep -> {
+                       pose = Pose2d(pose.x, pose.y, currentStateStep.motionProfile[currentStateOffset].x).toMeepMeepPose()
+                    }
+                }
+            } else if(looping) {
+                trajectorySequenceElapsedTime = 0.0
+            } else {
+                trajectorySequenceElapsedTime = 0.0
+                currentTrajectorySequence = null
             }
         }
+    }
+
+    fun start() {
+        running = true
+        trajectorySequenceElapsedTime = 0.0
     }
 
     fun followTrajectorySequence(sequence: TrajectorySequence) {
